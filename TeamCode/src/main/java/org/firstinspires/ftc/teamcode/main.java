894a4f9bcfef4f56a10444f2bad02f6d3d2d57c1
chinmaySanklipur <chinmaysanklipur@gmail.com>
2025-10-28 23:41:34 -0500

    Basic Movement
    Still need to dial in the controllability of movement. Movement is extremely touchy rn.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/main.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/main.java
@@ -4,20 +4,52 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp
-public class void mainDrive extends OpMode()
-{
+public class main extends LinearOpMode {
     @Override
-    init()
-    {
+    public void runOpMode() {
+        // Initialize motors
+        // Front Left has the green sticker near it
+        // All other motors
+        DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontLeft");
+        DcMotor backLeftMotor = hardwareMap.dcMotor.get("backLeft");
+        DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRight");
+        DcMotor backRightMotor = hardwareMap.dcMotor.get("backRight");
 
+        //Have to set right motors reverse for mecanum
+        frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            double y = -gamepad1.left_stick_y;
+            double x = gamepad1.left_stick_x;
+            double rx = gamepad1.right_stick_x;
+
+            // Calculate motor powers
+            double frontLeftPower = y + x + rx;
+            double backLeftPower = y - x + rx;
+            double frontRightPower = y - x - rx;
+            double backRightPower = y + x - rx;
+
+            // Normalize motor powers
+            double maxMagnitude = Math.max(Math.abs(frontLeftPower), Math.max(Math.abs(backLeftPower), Math.max(Math.abs(frontRightPower), Math.abs(backRightPower))));
+            if (maxMagnitude > 1) {
+                frontLeftPower /= maxMagnitude;
+                backLeftPower /= maxMagnitude;
+                frontRightPower /= maxMagnitude;
+                backRightPower /= maxMagnitude;
+            }
+
+            // Set motor powers
+            frontLeftMotor.setPower(frontLeftPower);
+            backLeftMotor.setPower(backLeftPower);
+            frontRightMotor.setPower(frontRightPower);
+            backRightMotor.setPower(backRightPower);
+        }
     }
-    public void runOpMode() { // EDIT
-    leftmotor = hardwareMap.get(DcMotorEx.class, "leftmotor");
-    rightmotor = hardwareMap.get(DcMotorEx.class, "rightmotor");
-
-    leftmotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-    rightmotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 }
